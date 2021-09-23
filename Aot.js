d841b66c973624ddb1eb89cedb019406bc6ade22
bharats9768 <74733419+bharats9768@users.noreply.github.com>
2021-09-24 04:00:42 +0530

    AOT

--- Aot.js
+++ Aot.js
@@ -2,13 +2,15 @@ const areaInputs = document.querySelectorAll(".triangle-area");
 const submitAreaBtn = document.querySelector("#submit-answer-btn");
 const outputEl = document.querySelector("#output-btn");
 
-function calculateSumOfBH (a, b, c){
+function calculateSumOfBH(a, b, c) {
     const sumOfBaseAndHeight = a * b * c;
     return sumOfBaseAndHeight;
 }
 
-function calculateAreaOfTriangle (){
-    const sumOfBaseAndHeight = calculateSumOfBH(Number(areaInputs[0].value),Number(areaInputs[1].value), Number(areaInputs[2].value));
+function calculateAreaOfTriangle() {
+    const sumOfBaseAndHeight = calculateSumOfBH(Number(areaInputs[0].value), Number(areaInputs[1].value), Number(areaInputs[2].value));
+    const lengthOfTriangle = Math.sqrt(sumOfBaseAndHeight)
+    outputEl.innerText = "correct"
     console.log(sumOfBaseAndHeight);
 }
 
