b99b83c3c7ab0d981165f5ca79b378ff8bb0cf87
bharats9768 <74733419+bharats9768@users.noreply.github.com>
2021-09-24 22:01:19 +0530

    js updated

--- Aot.js
+++ Aot.js
@@ -1,18 +1,21 @@
-const areaInputs = document.querySelectorAll(".triangle-area");
+const inputs = document.querySelectorAll(".triangle-area");
 const submitAreaBtn = document.querySelector("#submit-answer-btn");
 const outputEl = document.querySelector("#output-btn");
 
-function calculateSumOfBH(a, b, c) {
-    const sumOfBaseAndHeight = a * b * c;
-    return sumOfBaseAndHeight;
+function calculateOfFormula(base, height) {
+    const multiplyOfBh = base * height;
+    return multiplyOfBh;
 }
 
 function calculateAreaOfTriangle() {
-    const sumOfBaseAndHeight = calculateSumOfBH(Number(areaInputs[0].value), Number(areaInputs[1].value), Number(areaInputs[2].value));
-    const lengthOfTriangle = Math.sqrt(sumOfBaseAndHeight)
-    outputEl.innerText = "correct"
-    console.log(sumOfBaseAndHeight);
+    const baseAndHeight = calculateOfFormula(inputs[0].value, inputs[1].value);
+    const area = baseAndHeight * 0.5
+    console.log(area);
+    outputEl.innerText = "The area of triangle is " + area;
+    
 }
 
 
-submitAreaBtn.addEventListener("click", calculateSumOfBH);
+
+
+submitAreaBtn.addEventListener("click", calculateAreaOfTriangle)
